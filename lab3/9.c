ab6620a80f65f459cca7123c512a644ebb0e29e1
ARYAN-PATEL-11 <74341645+ARYAN-PATEL-11@users.noreply.github.com>
2023-09-08 19:54:15 +0530

    Header Updated

--- lab3/9.c
+++ lab3/9.c
@@ -1,3 +1,21 @@
+/*
+============================================================================
+Name : 9.c
+Author : Aryan Patel 
+Description : 9. Write a program to print the following information about a given file.
+            a. inode
+            b. number of hard links
+            c. uid
+            d. gid
+            e. size
+            f. block size
+            g. number of blocks
+            h. time of last access
+            i. time of last modification
+            j. time of last change         
+Date: 24 Aug, 2023.
+============================================================================
+*/
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -6,12 +24,10 @@
 
 void printFileInformation(const char *filename) {
     struct stat fileInfo;
-
     if (stat(filename, &fileInfo) == -1) {
         perror("Error getting file information");
         return;
     }
-
     printf("File: %s\n", filename);
     printf("Inode number: %ld\n", (long)fileInfo.st_ino);
     printf("Number of hard links: %ld\n", (long)fileInfo.st_nlink);
@@ -24,14 +40,11 @@ void printFileInformation(const char *filename) {
     printf("Time of last modification: %s", ctime(&fileInfo.st_mtime));
     printf("Time of last change: %s", ctime(&fileInfo.st_ctime));
 }
-
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         printf("Provide exactly 2 params");
         return 1;
     }
-
     printFileInformation(argv[1]);
-
     return 0;
 }
