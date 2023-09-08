c5fb39a4580ab2adf161463591e135847ff48c71
ARYAN-PATEL-11 <74341645+ARYAN-PATEL-11@users.noreply.github.com>
2023-09-08 23:59:31 +0530

    Header Updated

--- lab3/12.c
+++ lab3/12.c
@@ -1,28 +1,34 @@
+/*
+============================================================================
+Name : 12.c
+Author : Aryan Patel
+Description : 12. Write a program to find out the opening mode of a file. Use fcntl.          
+Date: 24 Aug, 2023.
+============================================================================
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-int main(int argc , char* argv[]) {
-    if(argc!=2){
+int main(void) {
+    int fileMode;
+    int fd = open("12.txt", O_RDONLY);
+    fileMode = fcntl(fd, F_GETFL);
+    if (fileMode == -1)
+    {
         printf("You are useless");
         return 0;
     }
-    int flags;
-    const char *file_path = argv[1];
-    int fd = open(file_path, O_WRONLY);//O_RDONLY,O_WRONLY,O_RDWR
-    flags = fcntl(fd, F_GETFL); /* Third argument is not required */
-    if (flags == -1){
-        printf("You are useless");
-        return 0;
-    }
-// Check opening modes
-    if ((flags & O_ACCMODE) == O_RDONLY) {
+    if ((fileMode & O_ACCMODE) == O_RDONLY)
+    {
         printf("File opened in read-only mode.\n");
     }
-    if ((flags & O_ACCMODE) == O_WRONLY) {
+    if ((fileMode & O_ACCMODE) == O_WRONLY)
+    {
         printf("File opened in write-only mode.\n");
     }
-    if ((flags & O_ACCMODE) == O_RDWR) {
+    if ((fileMode & O_ACCMODE) == O_RDWR) 
+    {
         printf("File opened in read-write mode.\n");
     }
     close(fd);
