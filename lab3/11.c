f59cab057779ac0c65dad25a5b074ee22ea08d8a
ARYAN-PATEL-11 <74341645+ARYAN-PATEL-11@users.noreply.github.com>
2023-09-08 23:52:47 +0530

    Header Updated

--- lab3/11.c
+++ lab3/11.c
@@ -2,9 +2,7 @@
 ============================================================================
 Name : 11.c
 Author : Aryan Patel 
-Description : 
-        11. Write a program to open a file, duplicate the file descriptor and append the file with both the
-        descriptors and check whether the file is updated properly or not.
+Description : 11. Write a program to open a file, duplicate the file descriptor and append the file with both the descriptors and check whether the file is updated properly or not.
           a. use dup
           b. use dup2
           c. use fcntl           
