e29bb8aa26abca0446105b00fe1a934183b89efc
ARYAN-PATEL-11 <74341645+ARYAN-PATEL-11@users.noreply.github.com>
2023-09-08 16:15:06 +0530

    Header Updated

--- lab2/3.c
+++ lab2/3.c
@@ -1,24 +1,23 @@
+/*
+============================================================================
+Name : 3.c
+Author : Aryan Patel
+Description : 3. Write a program to create a file and print the file descriptor value. Use creat ( ) system call      
+Date: 11 Aug, 2023.
+============================================================================
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-
 int main() {
-    // Path to the file to be created
     const char* filename = "my_file.txt";
-
-    // Create the file using creat() system call
     int fd = creat(filename, 0644);  // 0644 represents file permissions
-
     if (fd == -1) {
         perror("Error creating file");
         return 1;
     }
-
     printf("File created successfully with file descriptor: %d\n", fd);
-
-    // Close the file descriptor
     close(fd);
-
     return 0;
 }
