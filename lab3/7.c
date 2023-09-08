9827677fec4470fca8e9efd294e878d48d04217c
ARYAN-PATEL-11 <74341645+ARYAN-PATEL-11@users.noreply.github.com>
2023-09-08 23:55:39 +0530

    Header Updated

--- lab3/7.c
+++ lab3/7.c
@@ -2,8 +2,7 @@
 ============================================================================
 Name : 7.c
 Author : Aryan Patel
-Description : 
-        7. Write a program to copy file1 into file2 ($cp file1 file2).           
+Description : 7. Write a program to copy file1 into file2 ($cp file1 file2).           
 Date: 18 Aug, 2023.
 ============================================================================
 */
