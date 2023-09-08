32d29ac0c82167371f60f719aa36ad39c36fc0b8
ARYAN-PATEL-11 <74341645+ARYAN-PATEL-11@users.noreply.github.com>
2023-09-08 23:51:41 +0530

    Header Updated

--- lab3/11.c
+++ lab3/11.c
@@ -1,25 +1,32 @@
+/*
+============================================================================
+Name : 11.c
+Author : Aryan Patel 
+Description : 
+        11. Write a program to open a file, duplicate the file descriptor and append the file with both the
+        descriptors and check whether the file is updated properly or not.
+          a. use dup
+          b. use dup2
+          c. use fcntl           
+Date: 25 Aug, 2023.
+============================================================================
+*/
 #include<unistd.h>
 #include<fcntl.h>
 #include<stdio.h>
 #include<string.h>
-
 int main(int argc, char* argv[]){
-
    if(argc!=3){
      printf("missing arguments");
      return 1;
    }
-   
-   //opening a file
    int fd = open(argv[1],O_RDWR|O_CREAT|O_TRUNC|O_APPEND,0644);
    if(fd==-1){
      perror("Error opening a file");
      return 1;
    }
-   
    char a[sizeof(argv[1])];
    strncpy(a,argv[2],sizeof(argv[2]));
-   
    // appending data into the file using our descriptor
    ssize_t bytes = write(fd,a,sizeof(a)-1);
    if(bytes==-1){
@@ -27,9 +34,7 @@ int main(int argc, char* argv[]){
      close(fd);
      return 1;
    }
-   
    int new_fd1 = dup(fd);
-   
    // appending data into the file using dup()
    bytes = write(new_fd1,a,sizeof(a)-1);
    if(bytes==-1){
@@ -40,9 +45,7 @@ int main(int argc, char* argv[]){
    else{
      printf("Appended data successfully using dup() file desc");
    }
-   
    int new_fd2 = dup2(fd,4);
-   
    //appending data into the file using dup2()
    bytes = write(new_fd2,a,sizeof(a)-1);
    if(bytes==-1){
@@ -53,10 +56,7 @@ int main(int argc, char* argv[]){
    else{
      printf("\nAppended data successfully using dup2() file desc");
    }
-   
-   
    int new_fd3 = fcntl(fd,F_DUPFD,5);
-   
    //appending data into the file using fcntl()
    bytes = write(new_fd2,a,sizeof(a)-1);
    if(bytes==-1){
@@ -66,9 +66,7 @@ int main(int argc, char* argv[]){
    } 
    else{
      printf("\nAppended data successfully using fcntl file desc");
-
    }
-   
    int close_fd  = close(fd);
    if(close_fd==-1){
      perror("Error closing the file");
