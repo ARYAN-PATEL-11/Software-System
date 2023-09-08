7a9bdba07af16041711b014afd3acd519879b7fc
ARYAN-PATEL-11 <74341645+ARYAN-PATEL-11@users.noreply.github.com>
2023-09-08 16:21:46 +0530

    Header Updated

--- lab2/5.c
+++ lab2/5.c
@@ -1,3 +1,12 @@
+/*
+============================================================================
+Name : 5.c
+Author : Aryan Patel
+Description : 5. Write a program to create five new files with infinite loop. Execute the program in the background
+and check the file descriptor table at /proc/pid/fd.           
+Date: 18th Aug, 2023.
+============================================================================
+*/
 #include<stdio.h>
 #include<fcntl.h>
 int main(){
