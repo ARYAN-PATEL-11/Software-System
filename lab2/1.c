cafffe25cd6c5f738721c1217e72b9abef4f8d76
ARYAN-PATEL-11 <74341645+ARYAN-PATEL-11@users.noreply.github.com>
2023-09-08 16:19:03 +0530

    Header Updated

--- lab2/1.c
+++ lab2/1.c
@@ -1,10 +1,20 @@
+/*
+============================================================================
+Name : 1.c
+Author : Aryan Patel
+Description : 1. Create the following types of a files using (i) shell command (ii) system call
+                a. soft link (symlink system call)
+                b. hard link (link system call)
+                c. FIFO (mkfifo Library Function or mknod system call           
+Date: 11th Aug, 2023.
+============================================================================
+*/
 #include<stdio.h>
 #include <unistd.h>
 #include<sys/types.h>
 #include<sys/stat.h>
 #include<fcntl.h>
 int main(){
-
 int v  = symlink("dest","destSL");
 if(v<0)
 {perror("Failed ");return 1;}
