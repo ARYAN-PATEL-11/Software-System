121f3fd6b49876b4d839abf0f34028a3578301f5
ARYAN-PATEL-11 <74341645+ARYAN-PATEL-11@users.noreply.github.com>
2023-09-08 23:52:23 +0530

    Header Updated

--- lab3/10.c
+++ lab3/10.c
@@ -2,9 +2,7 @@
 ============================================================================
 Name : 10.c
 Author : Aryan Patel 
-Description : 
-        10. Write a program to open a file with read write mode, write 10 bytes, move the file pointer by 10
-        bytes (use lseek) and write again 10 bytes.
+Description : 10. Write a program to open a file with read write mode, write 10 bytes, move the file pointer by 10 bytes (use lseek) and write again 10 bytes.
             a. check the return value of lseek
             b. open the file with od and check the empty spaces in between the data.           
 Date: 18th Aug, 2023.
