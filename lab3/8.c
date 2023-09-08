cad10e2bbce546aeff47437231ddb7b59f1626f5
ARYAN-PATEL-11 <74341645+ARYAN-PATEL-11@users.noreply.github.com>
2023-09-08 19:55:59 +0530

    Header Updated

--- lab3/8.c
+++ lab3/8.c
@@ -1,8 +1,16 @@
+/*
+============================================================================
+Name : 8.c
+Author : Aryan Patel
+Description : 8. Write a program to open a file in read only mode, read line by line and display each line as it is read.
+        Close the file when end of file is reached.           
+Date: 18 Aug, 2023.
+============================================================================
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-
 int main(int argc , char* argv[]) {
     int fd;
     char filename[100];
@@ -12,14 +20,11 @@ int main(int argc , char* argv[]) {
         printf("You are useless");
         return 0;
     }
-
-    // Open the file in read-only mode
     fd = open(argv[1],O_RDONLY); 
     if (fd == -1) {
         perror("Error opening file");
         return 1;
     }
-
     // Read and display each line from the file
     while ((bytesRead = read(fd, buffer, sizeof(buffer))) > 0) {
         if (buffer[0] == '\n') {
@@ -28,15 +33,11 @@ int main(int argc , char* argv[]) {
             write(STDOUT_FILENO, buffer, 1);
         }
     }
-
     if (bytesRead == -1) {
         perror("Error reading file");
         close(fd);
         return 1;
     }
-
-    // Close the file
     close(fd);
-
     return 0;
 }
