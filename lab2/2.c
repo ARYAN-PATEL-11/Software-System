c5cf5513022bc456dd31f084d3291b3c4c826c17
ARYAN-PATEL-11 <74341645+ARYAN-PATEL-11@users.noreply.github.com>
2023-09-08 16:17:29 +0530

    Header Updated

--- lab2/2.c
+++ lab2/2.c
@@ -1,4 +1,12 @@
-#include<stdio.h>
+/*
+============================================================================
+Name : 2.c
+Author : Aryan Patel
+Description : 2. Write a simple program to execute in an infinite loop at the background. Go to /proc directory and
+        identify all the process related information in the corresponding proc directory.           
+Date: 18th Aug, 2023.
+============================================================================
+*/#include<stdio.h>
 #include<fcntl.h>
 int main(){
   for(;;);
